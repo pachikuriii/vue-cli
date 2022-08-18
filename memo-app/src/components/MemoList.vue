fcc4706cdbda327302f6adf79e68a6a493bddb8f
pachikuriii <stakahashi0728@gmail.com>
2022-08-18 17:32:53 +0900

    メモの１行目だけ表示される状態に

---
+++ memo-app/src/components/MemoList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="memo_list">
+    <ul>
+      <li v-for="memo in memos" :key="memo.id">
+        {{ memo.content.split("\n")[0] }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MemoList",
+  computed: {
+    memos() {
+      return this.$store.state.memos;
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  display: flex;
+  flex-direction: column;
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
