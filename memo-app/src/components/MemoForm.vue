a76d39e6e403e1aeb086e3dad3268eb4faa73443
pachikuriii <stakahashi0728@gmail.com>
2022-08-18 17:37:24 +0900

    フォームを作成

---
+++ memo-app/src/components/MemoForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="memo_form">
+    <textarea
+      v-model="content"
+      type="text"
+      ref="comment"
+      rows="4"
+      cols="40"
+    ></textarea>
+    <button @click="save" class="button button-new">追加</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MemoForm",
+  data() {
+    return {
+      content: "",
+    };
+  },
+  methods: {
+    save() {
+      const memo = {
+        content: this.content,
+      };
+      this.$store.commit("save", memo);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
