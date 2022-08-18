30ba2f81a0e1b03bca4cbd471535d7c8d4404a05
pachikuriii <stakahashi0728@gmail.com>
2022-08-18 16:16:07 +0900

    メモにidが振れるように

--- memo-app/src/store/index.js
+++ memo-app/src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex'
+import { v4 as uuidv4 } from 'uuid'
 
 export default createStore({
     state: {
@@ -6,6 +7,7 @@ export default createStore({
     },
     mutations: {
         save(state, newMemo) {
+            newMemo.id = uuidv4()
             state.memos.unshift(newMemo)
       }
   }
